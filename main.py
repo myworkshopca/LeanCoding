12ec781308f38a3100deaef344edad31d3118d1a
seanmyworkshop <sean.chen@myworkshop.ca>
2021-01-23 19:53:39 +0000

    My first challenge!

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-name = input("What is your name? ")
-print("Hello ", name, "!")
+print("Welcome to me greeting program!")
+name = input("What is your name?")
+print("Hi ", name, ", Nice to meet you!")
+input("Press Enter to exit!")
