a3bf7e1e69246b02bfd6ce9a8849a44e3ae28a38
seanmyworkshop <sean.chen@myworkshop.ca>
2021-01-23 20:41:47 +0000

    add more files to play with

---
+++ types.py
@@ -0,0 +1,19 @@
+a = "123"
+b = int(a)
+print("a = ", a)
+print("b = ", b)
+
+print( 35 / 2 )
+print( 35 // 2)
+
+name = input("what is your name?")
+age = int(input("How old are your?"))
+
+who = input("Who are you?")
+if who == "dog":
+  print("bark!")
+elif who == "cat":
+  print("meow!")
+else:
+  print("I don't know you!")
+print("Press Enter key to exist!")
