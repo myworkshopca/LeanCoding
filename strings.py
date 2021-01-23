69c79eb77ac90fd1ce9d7c9477f9c323fac1459b
seanmyworkshop <sean.chen@myworkshop.ca>
2021-01-23 20:19:31 +0000

    play around strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+a = 'Hello'
+b = "hello"
+c = """hello
+Next line
+3rd license
+                        ---- again
+      a little shorter!
+"""
+print(c)
+# name = input("What is your name?\n:")
+print(c.upper())
+print(c.lower())
+print(c.title())
+print(c.capitalize())
+print(a * 10)
