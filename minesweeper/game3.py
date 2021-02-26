1ede1da2f4f762a1b716ff8301f391814e99d36d
seanmyworkshop <sean.chen@myworkshop.ca>
2021-02-26 01:44:23 +0000

    add new file.

---
+++ minesweeper/game3.py
@@ -0,0 +1,20 @@
+import curses
+
+def paintfield(stdscr, center, size):
+
+  for y in range(center[0] - size[0] // 2, center[0] + size[0] // 2):
+    for x in range(center[1] - size[1], center[1] + size[1], 2):
+      stdscr.addstr(y, x, chr(9608))
+
+def sweeper(stdscr):
+
+  sh, sw = stdscr.getmaxyx()
+  center = [sh // 2, sw // 2]
+
+  size = [6, 12]
+
+  paintfield(stdscr, center, size)
+
+  stdscr.getch()
+
+curses.wrapper(sweeper)
